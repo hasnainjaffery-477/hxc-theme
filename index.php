3e758766e16bce60ed89f73111c2bbddfd20df79
Hasnain jaffery <hasnainjaffery1@gmail.com>
2024-01-05 00:08:48 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// main template file
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hxc theme</title>
+</head>
+<body>
+    
+</body>
+</html>
